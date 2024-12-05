495d7a580ad0a2ee4d09fd931481ba910a0077f7
Kevin Nguyen <kevin.nguyen2321@yahoo.com>
2024-12-05 10:32:23 -0600

    Completed all required features

--- client/src/home/Home.jsx
+++ client/src/home/Home.jsx
@@ -1,9 +1,15 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 export const Home = () => {
+  const navigate = useNavigate();
+  const handleNewCustomerClick = () => {
+    navigate('customers/new');
+  };
+
   return (
     <div>
       <div>
+        <button onClick={handleNewCustomerClick}>Add new customer</button>
         <h2>Welcome to Hillary's Hair Care Salon</h2>
         <Outlet />
       </div>
